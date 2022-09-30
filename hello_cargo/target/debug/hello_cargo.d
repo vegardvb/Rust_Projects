c9038db2bcb8e12a22dedffd8798d38f07688b9c
Vegard Vaeng Bernhardsen <vegardvb@gmail.com>
2022-09-30 11:36:38 +0200

    kode 2

---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\Users\vegar\Rust_Projects\Project1\hello_cargo\target\debug\hello_cargo.exe: C:\Users\vegar\Rust_Projects\Project1\hello_cargo\src\main.rs
---
+++ hello_cargo/target/release/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\Users\vegar\Rust_Projects\Project1\hello_cargo\target\release\hello_cargo.exe: C:\Users\vegar\Rust_Projects\Project1\hello_cargo\src\main.rs
